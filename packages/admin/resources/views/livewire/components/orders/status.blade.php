45e834ea3d853f9853f37fdefcc023c83e6d515f
Alec Ritson <hello@itsalec.co.uk>
2022-06-27 10:36:50 +0100

    feat/order-notifications

--- packages/admin/resources/views/livewire/components/orders/status.blade.php
+++ packages/admin/resources/views/livewire/components/orders/status.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <x-hub::button theme="gray" size="sm" type="button" wire:click.prevent="$set('showStatusSelect', true)">
+    <button type="button" class="inline-flex items-center px-4 py-2 font-bold transition border border-transparent border-gray-200 rounded hover:bg-white bg-gray-50 hover:border-gray-200" theme="gray" size="sm" type="button" wire:click.prevent="$set('showStatusSelect', true)">
         Update Status
-    </x-hub::button>
+    </button>
 
     <x-hub::slideover title="Update status" form="updateStatus" wire:model="showStatusSelect">
         <div class="space-y-4">
