ff43205c80f3d60ccec6446c2c97b29f059faf67
Alec Ritson <hello@itsalec.co.uk>
2022-06-28 14:14:35 +0100

    Don’t pass options

--- packages/admin/resources/views/components/input/datepicker.blade.php
+++ packages/admin/resources/views/components/input/datepicker.blade.php
@@ -14,7 +14,7 @@
     x-ref="input"
     type="text"
     x-bind:value="value"
-    {{ $attributes->whereDoesntStartWith('wire:model') }}
+    {{ $attributes->whereDoesntStartWith(['wire:model', 'options']) }}
   />
   <div x-show="value" class="absolute right-0 mr-3">
     <button x-on:click="value = null" type="button" class="inline-flex items-center text-sm text-gray-400 hover:text-gray-800">
