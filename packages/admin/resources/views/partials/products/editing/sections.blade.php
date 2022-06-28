ceba19674d9f414b04025f13cdf8daf9ae5791a3
markmead <itsmarkmead@gmail.com>
2022-06-28 16:48:48 +0100

    Updated - Use components to DRY up code

--- packages/admin/resources/views/partials/products/editing/sections.blade.php
+++ packages/admin/resources/views/partials/products/editing/sections.blade.php
@@ -29,7 +29,7 @@
         </div>
     </form>
 
-    <div class="pb-32 mt-8 lg:gap-8 lg:flex">
+    <div class="pb-32 mt-8 lg:gap-8 lg:flex lg:items-start">
         <div class="space-y-6 lg:flex-1">
             <div>
                 @if (!$this->hasChannelAvailability)
@@ -173,91 +173,87 @@
             </div>
         </div>
 
-        <aside class="sticky hidden h-full lg:block lg:flex-shrink-0 top-8">
-            <div class="flex flex-col overflow-y-auto bg-white rounded-lg shadow sm:rounded-md w-72">
-                <div class="px-4 py-6">
-                    <nav class="space-y-2"
-                         aria-label="Sidebar"
-                         x-data="{ activeAnchorLink: '' }"
-                         x-init="activeAnchorLink = window.location.hash">
-                        @foreach ($this->getSlotsByPosition('top') as $slot)
-                            <a href="#{{ $slot->handle }}"
-                               @class([
-                                   'flex items-center gap-2 p-2 rounded text-gray-500',
-                                   'hover:bg-blue-50 hover:text-blue-700' => empty(
-                                       $this->getSlotErrorsByHandle($slot->handle)
-                                   ),
-                                   'text-red-600' => !empty($this->getSlotErrorsByHandle($slot->handle)),
-                               ])
-                               aria-current="page"
-                               x-data="{ linkId: '#{{ $item['id'] }}' }"
-                               :class="{
-                                   'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
-                               }"
-                               x-on:click="activeAnchorLink = linkId">
-                                @if (!empty($this->getSlotErrorsByHandle($slot->handle)))
-                                    <x-hub::icon ref="exclamation-circle"
-                                                 class="w-4 mr-1 text-red-600" />
-                                @endif
+        <x-hub::page-menu-layout>
+            <nav class="space-y-2"
+                 aria-label="Sidebar"
+                 x-data="{ activeAnchorLink: '' }"
+                 x-init="activeAnchorLink = window.location.hash">
+                @foreach ($this->getSlotsByPosition('top') as $slot)
+                    <a href="#{{ $slot->handle }}"
+                       @class([
+                           'flex items-center gap-2 p-2 rounded text-gray-500',
+                           'hover:bg-blue-50 hover:text-blue-700' => empty(
+                               $this->getSlotErrorsByHandle($slot->handle)
+                           ),
+                           'text-red-600' => !empty($this->getSlotErrorsByHandle($slot->handle)),
+                       ])
+                       aria-current="page"
+                       x-data="{ linkId: '#{{ $item['id'] }}' }"
+                       :class="{
+                           'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
+                       }"
+                       x-on:click="activeAnchorLink = linkId">
+                        @if (!empty($this->getSlotErrorsByHandle($slot->handle)))
+                            <x-hub::icon ref="exclamation-circle"
+                                         class="w-4 mr-1 text-red-600" />
+                        @endif
 
-                                <span class="text-sm font-medium">
-                                    {{ $slot->title }}
-                                </span>
-                            </a>
-                        @endforeach
+                        <span class="text-sm font-medium">
+                            {{ $slot->title }}
+                        </span>
+                    </a>
+                @endforeach
 
-                        @foreach ($this->sideMenu as $item)
-                            <a href="#{{ $item['id'] }}"
-                               @class([
-                                   'flex items-center gap-2 p-2 rounded text-gray-500',
-                                   'hover:bg-blue-50 hover:text-blue-700' => empty($item['has_errors']),
-                                   'text-red-600 bg-red-50' => !empty($item['has_errors']),
-                               ])
-                               aria-current="page"
-                               x-data="{ linkId: '#{{ $item['id'] }}' }"
-                               :class="{
-                                   'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
-                               }"
-                               x-on:click="activeAnchorLink = linkId">
-                                @if (!empty($item['has_errors']))
-                                    <x-hub::icon ref="exclamation-circle"
-                                                 class="w-4 text-red-600" />
-                                @endif
+                @foreach ($this->sideMenu as $item)
+                    <a href="#{{ $item['id'] }}"
+                       @class([
+                           'flex items-center gap-2 p-2 rounded text-gray-500',
+                           'hover:bg-blue-50 hover:text-blue-700' => empty($item['has_errors']),
+                           'text-red-600 bg-red-50' => !empty($item['has_errors']),
+                       ])
+                       aria-current="page"
+                       x-data="{ linkId: '#{{ $item['id'] }}' }"
+                       :class="{
+                           'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
+                       }"
+                       x-on:click="activeAnchorLink = linkId">
+                        @if (!empty($item['has_errors']))
+                            <x-hub::icon ref="exclamation-circle"
+                                         class="w-4 text-red-600" />
+                        @endif
 
-                                <span class="text-sm font-medium">
-                                    {{ $item['title'] }}
-                                </span>
-                            </a>
-                        @endforeach
+                        <span class="text-sm font-medium">
+                            {{ $item['title'] }}
+                        </span>
+                    </a>
+                @endforeach
 
-                        @foreach ($this->getSlotsByPosition('bottom') as $slot)
-                            <a href="#{{ $slot->handle }}"
-                               @class([
-                                   'flex items-center gap-2 p-2 rounded text-gray-500',
-                                   'hover:bg-blue-50 hover:text-blue-700' => empty(
-                                       $this->getSlotErrorsByHandle($slot->handle)
-                                   ),
-                                   'text-red-600' => !empty($this->getSlotErrorsByHandle($slot->handle)),
-                               ])
-                               aria-current="page"
-                               x-data="{ linkId: '#{{ $item['id'] }}' }"
-                               :class="{
-                                   'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
-                               }"
-                               x-on:click="activeAnchorLink = linkId">
-                                @if (!empty($this->getSlotErrorsByHandle($slot->handle)))
-                                    <x-hub::icon ref="exclamation-circle"
-                                                 class="w-4 mr-1 text-red-600" />
-                                @endif
+                @foreach ($this->getSlotsByPosition('bottom') as $slot)
+                    <a href="#{{ $slot->handle }}"
+                       @class([
+                           'flex items-center gap-2 p-2 rounded text-gray-500',
+                           'hover:bg-blue-50 hover:text-blue-700' => empty(
+                               $this->getSlotErrorsByHandle($slot->handle)
+                           ),
+                           'text-red-600' => !empty($this->getSlotErrorsByHandle($slot->handle)),
+                       ])
+                       aria-current="page"
+                       x-data="{ linkId: '#{{ $item['id'] }}' }"
+                       :class="{
+                           'bg-blue-50 text-blue-700 hover:text-blue-600': linkId === activeAnchorLink
+                       }"
+                       x-on:click="activeAnchorLink = linkId">
+                        @if (!empty($this->getSlotErrorsByHandle($slot->handle)))
+                            <x-hub::icon ref="exclamation-circle"
+                                         class="w-4 mr-1 text-red-600" />
+                        @endif
 
-                                <span class="text-sm font-medium">
-                                    {{ $slot->title }}
-                                </span>
-                            </a>
-                        @endforeach
-                    </nav>
-                </div>
-            </div>
-        </aside>
+                        <span class="text-sm font-medium">
+                            {{ $slot->title }}
+                        </span>
+                    </a>
+                @endforeach
+            </nav>
+        </x-hub::page-menu-layout>
     </div>
 </div>
