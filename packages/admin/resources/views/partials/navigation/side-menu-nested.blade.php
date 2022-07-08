68009dad74be948375fd1b414d8eff8ade61fb5f
Alec Ritson <hello@itsalec.co.uk>
2022-07-08 10:43:43 +0100

    Allow sections in side menu

--- packages/admin/resources/views/partials/navigation/side-menu-nested.blade.php
+++ packages/admin/resources/views/partials/navigation/side-menu-nested.blade.php
@@ -1,27 +1,61 @@
 <x-hub::layout.side-menu>
     <x-hub::menu handle="{{ $menu }}"
                  current="{{ request()->route()->getName() }}">
-        <ul class="space-y-2">
-            @foreach ($component->items as $item)
-                <li>
-                    <a href="{{ route($item->route) }}"
-                       @class([
-                           'flex items-center gap-2 p-2 rounded text-gray-500',
-                           'bg-blue-50 text-blue-700 hover:text-blue-600' => $item->isActive(
-                               $component->attributes->get('current')
-                           ),
-                           'hover:bg-blue-50 hover:text-blue-700' => !$item->isActive(
-                               $component->attributes->get('current')
-                           ),
-                       ])>
-                        {!! $item->renderIcon('shrink-0 w-5 h-5') !!}
 
-                        <span class="text-sm font-medium">
-                            {{ $item->name }}
-                        </span>
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+        <div class="space-y-4">
+          @if($component->items->count())
+            <ul class="space-y-2">
+                @foreach ($component->items as $item)
+                    <li>
+                        <a href="{{ route($item->route) }}"
+                           @class([
+                               'flex items-center gap-2 p-2 rounded text-gray-500',
+                               'bg-blue-50 text-blue-700 hover:text-blue-600' => $item->isActive(
+                                   $component->attributes->get('current')
+                               ),
+                               'hover:bg-blue-50 hover:text-blue-700' => !$item->isActive(
+                                   $component->attributes->get('current')
+                               ),
+                           ])>
+                            {!! $item->renderIcon('shrink-0 w-5 h-5') !!}
+
+                            <span class="text-sm font-medium">
+                                {{ $item->name }}
+                            </span>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+          @endif
+          @foreach($component->sections as $section)
+            <div>
+              <header class="text-sm font-bold text-gray-600">
+                {{ $section->name }}
+              </header>
+              <ul class="space-y-2 mt-2">
+                  @foreach ($section->getItems() as $item)
+                      <li>
+                          <a href="{{ route($item->route) }}"
+                             @class([
+                                 'flex items-center gap-2 p-2 rounded text-gray-500',
+                                 'bg-blue-50 text-blue-700 hover:text-blue-600' => $item->isActive(
+                                     $component->attributes->get('current')
+                                 ),
+                                 'hover:bg-blue-50 hover:text-blue-700' => !$item->isActive(
+                                     $component->attributes->get('current')
+                                 ),
+                             ])>
+                              {!! $item->renderIcon('shrink-0 w-5 h-5') !!}
+
+                              <span class="text-sm font-medium">
+                                  {{ $item->name }}
+                              </span>
+                          </a>
+                      </li>
+                  @endforeach
+              </ul>
+            </div>
+          @endforeach
+        </div>
     </x-hub::menu>
 </x-hub::layout.side-menu>
